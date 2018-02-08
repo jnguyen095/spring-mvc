b6362fb1bcf0087a4295cdf0aaa8f5a3fd152912
Khang Nguyen <khang.nguyen@banvien.com>
2018-02-08 10:18:30 +0700

    Revise configuration

--- src/main/java/com/test/HomeController.java
+++ src/main/java/com/test/HomeController.java
@@ -2,7 +2,9 @@ package com.test;
 
 import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Khang Nguyen.
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController extends ApplicationObjectSupport{
 
     @RequestMapping("/home.html")
-    public String home(){
+    public String home(ModelMap model){
+         model.addAttribute("message", "Spring is running...");
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -6,11 +6,13 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <html>
 <head>
-    <title>Spring configuration</title>
+    <title>Spring Configuration</title>
 </head>
 <body>
-<h2>Spring is running...</h2>
+<h2>Message: <c:out value="${message}"/></h2>
 </body>
 </html>
